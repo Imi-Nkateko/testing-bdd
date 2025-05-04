c489228eece3f5b19b2eb2f6e0abe08ac0429a12
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-05-04 21:29:43 +0200

    complete test suit 1

--- test/ship_test.js
+++ test/ship_test.js
@@ -1,5 +1,16 @@
-let expect = require('chai').expect
+let expect = require("chai").expect;
 
 describe("check for ship", () => {
-    let checkForShip = require('../game_logic/ship_methods').checkForShip
-})
+  let checkForShip = require("../game_logic/ship_methods").checkForShip;
+
+  it("should correctly report no ship at a given players coordinate", () => {
+    player = {
+      ships: [
+        {
+          locations: [[0, 0]],
+        },
+      ],
+    };
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+});
