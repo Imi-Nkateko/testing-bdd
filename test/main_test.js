004571a909ff35678a0bfe239fb2ee450eb9cc17
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-05-04 21:01:19 +0200

    create main.js

---
+++ test/main_test.js
@@ -0,0 +1,12 @@
+// let expect = require("chai").expect
+
+
+// // test suit 
+
+
+// describe("Mocha", ()=> {
+//     // Test spec (unit test)
+//     it("should write tests using npm", () => {
+
+//     })
+// })
